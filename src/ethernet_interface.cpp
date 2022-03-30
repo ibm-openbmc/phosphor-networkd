098b332a1ac70aa84c469ba17e3e629a2692b0e1
Patrick Williams <patrick@stwcx.xyz>
2022-03-30 16:09:16 -0500

    sdbusplus: object: don't use 'bool' argument constructor
    `sdbusplus::server::object_t` has long had an enum-based parameter for
    signal action, but maintained a backwards compatible boolean mapping.
    It is time to remove this boolean to make it more observable which
    actions are being used in applications.  Map all `true` occurrences to
    `action::defer_emit`.
    
    Change-Id: I2e7ce5e279d65d101e1249f2b41f126143d3f03d
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/ethernet_interface.cpp
+++ src/ethernet_interface.cpp
@@ -82,7 +82,9 @@ EthernetInterface::EthernetInterface(sdbusplus::bus::bus& bus,
                                      const std::string& objPath,
                                      DHCPConf dhcpEnabled, Manager& parent,
                                      bool emitSignal) :
-    Ifaces(bus, objPath.c_str(), true),
+    Ifaces(bus, objPath.c_str(),
+           emitSignal ? Ifaces::action::defer_emit
+                      : Ifaces::action::emit_no_signals),
     bus(bus), manager(parent), objPath(objPath)
 {
     auto intfName = objPath.substr(objPath.rfind("/") + 1);
