81cf9b42a4715a01522315ccdba23a9d3ca2a9ea
Asmitha Karunanithi <47350623+asmithakarun@users.noreply.github.com>
2022-04-01 00:08:42 +0530

    Create hypervisor ip objects when biostable is null (#32)
    There wes a scenario where the hypervisor app starts
    even before the pldm populates the biostable. In this
    case, hypervisor app does not create any ip address
    objects.
    
    A solution to this is,
    * to create the ip address objects with default values,
    even if the biostable is empty;
    * then wait for properties changed signal on bios table
    and update the ip address dbus objects
    
    Thus, with this change, hypervisor app never fails to create
    ip dbus objects.
    
    Tested-By:
    
    * Bios table is empty
      start the app, with pldm already running
      hypervisor dbus objects gets created
    
    * Restart pldmd after the above testcase
      bios table gets initialised
      hyp app gets the properties changed signal
    
    * Host is powered off
      after the above test case, configured ip through Redfish
      redfish static ip configuration - returns internal error,
                                        as biostable is empty
    
    * Host powered off
      restart pldmd
      configure static ip through Redfish
      dbus object gets updated followed by bios table
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>
    Change-Id: I07aaf396af7abd4871edfdaff2ba4fa7abae1c01

--- src/ibm/hypervisor-network-mgr-src/hyp_network_manager.hpp
+++ src/ibm/hypervisor-network-mgr-src/hyp_network_manager.hpp
@@ -105,6 +105,22 @@ class HypNetworkMgr
      */
     ethIntfMapType getEthIntfList();
 
+    /** @brief Method to set all the interface 0 attributes
+     *         to its default value in biosTableAttrs data member
+     */
+    void setIf0DefaultBIOSTableAttrs();
+
+    /** @brief Method to set all the interface 1 attributes
+     *         to its default value in biosTableAttrs data member
+     */
+    void setIf1DefaultBIOSTableAttrs();
+
+    /** @brief Method to set the hostname attribute
+     *         to its default value in biosTableAttrs
+     *         data member
+     */
+    void setDefaultHostnameInBIOSTableAttrs();
+
   private:
     /**
      * @brief get Dbus Prop
