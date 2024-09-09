ba22fd37ca2b9fe275f7d913b89455ec196bd938
Eddie James <eajames@us.ibm.com>
2024-09-09 14:24:47 -0500

    Update NCSI timeout handler to unbind/bind the device driver (#148)
    The link down/link up didn't do the trick.
    
    Signed-off-by: Eddie James <eajames@linux.ibm.com>

--- src/ethernet_interface.hpp
+++ src/ethernet_interface.hpp
@@ -8,6 +8,7 @@
 #include "xyz/openbmc_project/Network/Neighbor/CreateStatic/server.hpp"
 #include "xyz/openbmc_project/Network/StaticRoute/Create/server.hpp"
 
+#include <filesystem>
 #include <optional>
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/server/object.hpp>
@@ -283,12 +284,11 @@ class EthernetInterface : public Ifaces
   private:
     struct NCSITimeoutWatch
     {
-        NCSITimeoutWatch(const std::string& ifname, int file);
+        NCSITimeoutWatch(EthernetInterface& intf, int fd);
 
         void callback(sdeventplus::source::IO&, int, uint32_t);
 
-        const std::string ifname;
-        stdplus::ManagedFd fd;
+        EthernetInterface& intf;
         sdeventplus::source::IO io;
     };
     std::unique_ptr<NCSITimeoutWatch> ncsiTimeoutWatch;
@@ -311,6 +311,14 @@ class EthernetInterface : public Ifaces
     /** @brief Vector of DHCP conf objects.
      */
     std::vector<std::unique_ptr<dhcp::Configuration>> dhcpConfigs;
+
+    int handleNCSITimeout();
+
+    std::filesystem::path ncsiTimeoutPath;
+    std::filesystem::path ncsiWatchDriver;
+    std::string ncsiWatchDeviceName;
+
+    friend struct NCSITimeoutWatch;
 };
 
 } // namespace network
