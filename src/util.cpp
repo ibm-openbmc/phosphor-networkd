0edb60525f222abd3c42e4a665a6346d855a8a68
raviteja-b <46921058+raviteja-b@users.noreply.github.com>
2024-12-18 20:24:34 +0530

    Add IPv4 routing tables for each ethernet interfaces (#166)
    This commit configures separate routing tables for each ethernet
    interface so that static gateway routes added on that interface
    (eth0/eth1) will be in added to separate routing tables
    
    Currently there are gateway routing issues when multiple interfaces
    configured in different subnets
    
    This commit fixes routing configuration issues when static addresses
    configured on eth0 and eth1 interfaces.
    
    Tested by:
    Ran CT/Automation network test cases
    Verified routing with eth0 static configuration and eth1 dhcp
    configuration
    Verified routing with eth0 DHCP configuration and eth1 static
    configuration
    Both eth0 and eth1 with static IP configurations
    
    Change-Id: If2103f8a850d554e51084f1ebe1bb15e616a8bec
    
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>

--- src/util.cpp
+++ src/util.cpp
@@ -225,6 +225,30 @@ bool getDHCPProp(const config::Parser& config, DHCPType dhcpType,
         .value_or(true);
 }
 
+std::string setIPv4AddressLastOctetToZero(const std::string& ip)
+{
+    std::vector<std::string> octets;
+    std::string octet;
+    std::stringstream ss(ip);
+
+    // Split the IP address by '.'
+    while (std::getline(ss, octet, '.'))
+    {
+        octets.push_back(octet);
+    }
+
+    // Modify the last octet
+    if (octets.size() == 4)
+    {
+        octets[3] = "0";
+    }
+
+    std::string modifiedIP = octets[0] + "." + octets[1] + "." + octets[2] +
+                             "." + octets[3];
+
+    return modifiedIP;
+}
+
 namespace mac_address
 {
 
