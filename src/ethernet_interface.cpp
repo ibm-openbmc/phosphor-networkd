4b1c2a085252f875b48337e0be1e06790b159927
Ravi Teja <raviteja28031990@gmail.com>
2025-07-22 10:47:34 -0500

    Fix IPv4 route destination for ethernet interfaces

--- src/ethernet_interface.cpp
+++ src/ethernet_interface.cpp
@@ -1191,12 +1191,6 @@ void EthernetInterface::writeConfigurationFile()
                         config.map["RoutingPolicyRule"].emplace_back();
                     routingPolicyFrom["Table"].emplace_back(routingTableId);
                     routingPolicyFrom["From"].emplace_back(routeAddressPrefix);
-                    auto& routingPolicyDestination =
-                        config.map["Route"].emplace_back();
-                    routingPolicyDestination["Table"].emplace_back(
-                        routingTableId);
-                    routingPolicyDestination["Destination"].emplace_back(
-                        routeAddressPrefix);
                 }
             }
 
