6e01cdffa6505c46ac424332e725600abf69b77e
Asmitha Karunanithi <asmitk01@in.ibm.com>
2026-01-20 06:38:11 -0600

    Allow hypervisor static ip config with 0.0.0.0 gw
    This commit allows users to configure static IP with 0.0.0.0 gateway.
    
    Tested By:
    
    [1] PATCH -d '{"IPv4StaticAddresses":[{"Address": "X.X.X.X","SubnetMask": "255.255.255.0","Gateway":"0.0.0.0"}]}' https://${bmc}/redfish/v1/Systems/hypervisor/EthernetInterfaces/eth0
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

--- src/ibm/hypervisor-network-mgr-src/hyp_ethernet_interface.cpp
+++ src/ibm/hypervisor-network-mgr-src/hyp_ethernet_interface.cpp
@@ -909,10 +909,6 @@ ObjectPath HypEthInterface::ip(HypIP::Protocol protType, std::string ipaddress,
                 validateGateway<stdplus::In4Addr>(gateway);
             }
         }
-        else
-        {
-            throw std::invalid_argument("Empty gateway");
-        }
     }
     catch (const std::exception& e)
     {
